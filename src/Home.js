8b009e8127e8777f85f92c9d0ed4e14c487fb1ad
Michael Kozubsi <mkozubski@academic.rrc.ca>
2022-05-10 20:55:04 -0500

    - Tutorial 14

--- src/Home.js
+++ src/Home.js
@@ -1,21 +1,28 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
         {title: 'My practice site', body: 'ipsum some text', author: 'Bubba Bodiddy', id: 1},
         {title: 'This is practice', body: 'ipsum some text', author: 'Wubly Doubly', id: 2},
-        {title: 'Even more practice', body: 'ipsum some text', author: 'Mama Mother', id: 1}
+        {title: 'Even more practice', body: 'ipsum some text', author: 'Mama Mother', id: 3}
         
     ]);
+
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
+    
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(blogs);
+    });
+
     return ( 
         <div className="home">
-            {blogs.map((blog) => (
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{ blog.title }</h2>
-                    <p>written by {blog.author}</p>
-                </div>
-            ))};
-
+            <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete}/>
+           {/*} <BlogList blogs={blogs.filter((blog) => blog.author === "Bubba Bodiddy")} title="Bubba's Blogs"/>{*/}
         </div>
      );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -3,7 +3,7 @@ const Navbar = () => {
         <nav className="navbar">
             <h1>The Port Blog</h1>
             <div className="links">
-                <a href="/">Home</a>
+                <a href="/" style={{color: "white", backgroundColor: "#f1356d", borderRadius: "8px"}}>Home</a>
                 <a href="/create" style={{color: "white", backgroundColor: "#f1356d", borderRadius: "8px"}}>New Blog</a>
             </div>
         </nav>
