f42985bcbf1ce639ac4b91486bcf7d5086d0b5a1
Michael Kozubsi <mkozubski@academic.rrc.ca>
2022-05-11 13:45:47 -0500

    -Added Routing

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return ( 
         <nav className="navbar">
             <h1>The Port Blog</h1>
             <div className="links">
-                <a href="/" style={{color: "white", backgroundColor: "#f1356d", borderRadius: "8px"}}>Home</a>
-                <a href="/create" style={{color: "white", backgroundColor: "#f1356d", borderRadius: "8px"}}>New Blog</a>
+                <Link to="/" style={{color: "white", backgroundColor: "#f1356d", borderRadius: "8px"}}>Home</Link>
+                <Link to="/create" style={{color: "white", backgroundColor: "#f1356d", borderRadius: "8px"}}>New Blog</Link>
             </div>
         </nav>
      );
